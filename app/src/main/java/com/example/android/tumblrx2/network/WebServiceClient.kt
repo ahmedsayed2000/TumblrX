b28427c38989215261e6f25b220e511b4dbbc83b
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2021-12-09 16:14:49 +0200

    splash screen bug fix / changed to deployed backend

--- app/src/main/java/com/example/android/tumblrx2/network/WebServiceClient.kt
+++ app/src/main/java/com/example/android/tumblrx2/network/WebServiceClient.kt
@@ -8,7 +8,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class WebServiceClient {
     companion object {
-        private const val BASE_URL = "http://10.0.2.2:4000/"
+//        private const val BASE_URL = "http://10.0.2.2:4000/"  //localhost backend - only works on emulator
+        private const val BASE_URL = "http://13.90.116.72:3000/" //deployed backend
     }
 
     fun <Api> buildApi(
