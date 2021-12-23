27f137bf0ae1ba980c9fbd858ae73ff0dc5848a6
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2021-12-23 16:52:00 +0200

    layout done

---
+++ app/src/main/java/com/example/android/tumblrx2/signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.tumblrx2.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.tumblrx2.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
